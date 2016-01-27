a37820c867f8d2eb9a8341aae00b2e425542c2ce
Ismael Castiñeira <ismael.castineira@students.turkuamk.fi>
2016-01-27 14:12:18 -0600

    Threads más claros

--- Laboratorio.java
+++ Laboratorio.java
@@ -12,27 +12,12 @@ class Laboratorio
 {
     public static void main(String arg[])
     {
-        while(true)
-        {
-            Pais thread1 = new Pais();
-            Hora thread2 = new Hora();
-            Mates thread3 = new Mates();
-            thread1.start();
-            thread2.start();
-            thread3.start();
-            try
-            {
-                while(true){
-                    thread1.join();
-                    thread2.join();
-                    thread3.join();
-                    break;
-                }
-            }
-            catch(InterruptedException e){
-            //nada 
-            }
-        }
+        Pais thread1 = new Pais();
+        Hora thread2 = new Hora();
+        Mates thread3 = new Mates();
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
 
@@ -40,17 +25,18 @@ class Pais extends Thread
 {
     public void run()
     {
-        Random rng = new Random();
-
-        try
-        {
-            Thread.sleep(rng.nextInt(1000));
+        while(true){
+            Random rng = new Random();
+            try
+            {
+                Thread.sleep(rng.nextInt(1000));
+            }
+            catch(InterruptedException e){
+            }
+            
+            String palabra = "Finland";
+            System.out.println(palabra); 
         }
-        catch(InterruptedException e){
-        }
-        
-        String palabra = "Finland";
-        System.out.println(palabra); 
     }
 }
 
@@ -59,21 +45,23 @@ class Hora extends Thread
 {
     public void run()
     {
-        Random rng = new Random();
-        
-        try
-        {
-            Thread.sleep(rng.nextInt(1000));
-        }
-        catch(InterruptedException e){
-        }
-        
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        while(true){
+            Random rng = new Random();
+            
+            try
+            {
+                Thread.sleep(rng.nextInt(1000));
+            }
+            catch(InterruptedException e){
+            }
+            
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss:SS");
 
-        
-        //get current date time with Calendar()
-        Calendar cal = Calendar.getInstance();
-        System.out.println(dateFormat.format(cal.getTime()));
+            
+            //get current date time with Calendar()
+            Calendar cal = Calendar.getInstance();
+            System.out.println(dateFormat.format(cal.getTime()));
+        }
     }
 }
 
@@ -81,15 +69,17 @@ class Mates extends Thread
 {
     public void run()
     {
-        Random rng = new Random();
         int a = 5;
-        for(int i = 0 ; i < 10 ; i++)
+        int i = 0;
+        while(true)
         {
-            System.out.println(a+" x "+i+" = "+i*a);
+            Random rng = new Random();
 
+            System.out.println(a+" x "+i+" = "+i*a);
+            i++;
             try
             {
-                Thread.sleep(rng.nextInt(100));
+                Thread.sleep(rng.nextInt(500));
             }
             catch(InterruptedException e){
             }
